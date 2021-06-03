53a1b3a7b6f887bef1f0a34718ccbc73f8c60230
Aleksey <titoff1995@gmail.com>
2021-06-03 14:57:14 +0300

    TJS-001 : task 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while ( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -8,55 +18,93 @@ const personalMovieDB = {
     actors : {},
     genres : [],
     private : false
-};
+};     
 
-// const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-//  ratingLastfilm = prompt('На сколько оцените его?', ''),
-//  lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-//  ratingLastfilm1 = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+            ratingLastfilm = prompt('На сколько оцените его?', '');
+            console.log(lastFilm);
+        if (lastFilm == null || ratingLastfilm == null || lastFilm == '' || ratingLastfilm == '' || lastFilm.length > 50) {
+            i--;
+            continue;
+        }
+        personalMovieDB.movies[lastFilm] = ratingLastfilm;
+    }
+}
 
-// personalMovieDB.movies[lastFilm] = ratingLastfilm;
-// personalMovieDB.movies[lastFilm1] = ratingLastfilm1;
+rememberMyFilms();
 
-// console.log(personalMovieDB);
+function decectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотренно довольно мало фильмов');
+   } else if (personalMovieDB.count < 30) {
+       alert('Вы классический зритель');
+   } else if (personalMovieDB.count > 30) {
+       alert('Вы киноман');
+   } else {
+       alert('Ошибка');
+   }
+}
+
+decectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        const favoriteGenre = prompt(`Ваш любимы жанр под номером ${i}`, '');
+        personalMovieDB.genres.push(favoriteGenre);
+    }
+}
+
+writeYourGenres();
+
+function showMyDB() {
+    if (!personalMovieDB.private) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
 
 
 
-// for(let i = 0; i < numberOfFilms; i++) {
-//     const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-//         ratingLastfilm = prompt('На сколько оцените его?', '');
-//     if (lastFilm.length == 0 || 
-//         ratingLastfilm.length == 0 || 
-//         lastFilm.length > 5021 || 
-//         ratingLastfilm.length > 50) {
-//         i--;
-//         continue;
-//     }
-//     personalMovieDB.movies[lastFilm] = ratingLastfilm;
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 30;
+//     console.log(num);
 // }
 
-for(let i = 0; i < 2; i++) {
-    const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-        ratingLastfilm = prompt('На сколько оцените его?', '');
-        console.log(lastFilm);
-    if (lastFilm == null ||
-        ratingLastfilm == null ||
-        lastFilm.length == 0 || 
-        ratingLastfilm.length == 0 || 
-        lastFilm.length > 50) {
-        i--;
-        continue;
-    }
-    personalMovieDB.movies[lastFilm] = ratingLastfilm;
-}
+// showFirstMessage('Hello World!');
+// console.log(num);
 
-if (personalMovieDB.count < 10) {
-     alert('Просмотренно довольно мало фильмов');
-}else if (personalMovieDB.count < 30) {
-    alert('Вы классический зритель');
-}else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-}else {
-    alert('Ошибка');
-}
-console.log(personalMovieDB);
+
+
+
+// const logger = function() {
+//     console.log('Hello');
+// };
+
+
+// logger();
+
+// const cals = (a, b) => { return a + b;};
+
+// console.log(cals(3,4));
+
+
+// let str = 'rweer';
+// const arr = [1, 2, 3];
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// // str = str.toUpperCase();
+// console.log(str);
+
+
+// const test = '12.3px';
+
+// console.log(parseFloat(test));
